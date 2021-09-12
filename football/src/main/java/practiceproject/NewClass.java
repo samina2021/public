f4f5189a326d2b4bb8df48e321bf4001efd06de8
ADMIN <ADMIN@QA-NINJA-1>
2021-09-11 23:17:04 -0400

    first Commit

---
+++ football/src/main/java/practiceproject/NewClass.java
@@ -0,0 +1,25 @@
+package practiceproject;
+
+public class NewClass{
+
+
+	public static void main(String[] args) {
+
+		tiger();
+NewClass object = new NewClass();
+object.bat();
+
+
+
+	}
+	public static void tiger() {
+		int car = 50;
+		System.out.println(car);
+		
+		
+	}
+	public void bat() {
+		String age ="my age is 63";
+		System.out.println(age);
+	}
+}
